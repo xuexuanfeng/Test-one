6cd15fb5a3e627681fc91f7532ddd0c6e886c851
xuexuanfeng <259363991@qq.com>
2025-08-10 23:59:02 +0800

    change3

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/test-one',
+  base: '/Test-one',
   css: {
     preprocessorOptions: {
       scss: {
